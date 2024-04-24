97427dfa1391bab5c62c08319c0269512d59fc4f
rojinatiwari <162018518+rojinatiwari@users.noreply.github.com>
2024-04-24 13:47:44 +0545

    Update Number_Guessing_Game.c

--- Number_Guessing_Game.c
+++ Number_Guessing_Game.c
@@ -7,7 +7,6 @@ int main()
     int number, guess, nguesses = 1;
     srand(time(0));
     number = rand() % 100 + 1; // generate random numer between 1 and 100
-    printf("The number is %d \n", number);
     // keep running the loop until the number is guessed
     do
     {
